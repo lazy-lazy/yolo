2d233ef9afb36ba756ef366cc8f71f944c22e3e8
丁强 <dq6663@ly.com>
2015-08-19 18:01:26 +0800

    添加post功能

--- routes/index.js
+++ routes/index.js
@@ -13,9 +13,50 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/login',isNotLogin);
 router.get('/login', function(req, res, next) {
-  res.render('login', { title: '登录' });
+  res.render('login', {
+    title: '登录',
+    user: req.session.user,
+    success: req.flash('success').toString(),
+    error: req.flash('error').toString()
+  });
 });
+
+router.post('/login',isNotLogin);
+router.post('/login',function(req,res,next){
+  //生成密码的MD5值
+  var md5 = crypto.createHash('md5'),
+      pwd = md5.update(req.body.password).digest('Hex');
+
+  //检查用户是否存在
+  User.get(req.body.name,function(err,user){
+    if(!user){
+      req.flash('error', '用户不存在！');
+      return res.redirect('/login');//用户不存在则跳转到登录页
+    }
+
+    //检查密码是否一致
+    if(user.password != pwd){
+      req.flash('error','密码错误！');
+      return res.redirect('/login');//密码错误跳转到登录页
+    }
+
+    //用户名和密码都匹配后，将用户信息存入 session
+    req.session.user= user;
+    req.flash('success','登录成功！');
+    res.redirect('/');//登录成功跳转到主页
+  });
+});
+
+router.get('/logout',isLogin);
+router.get('/logout',function(req,res,next){
+  req.session.user = null;
+  req.flash('success','登出成功！');
+  res.redirect('/');//登出成功后跳转到主页
+});
+
+router.get('/reg',isNotLogin);
 router.get('/reg', function(req, res, next) {
   res.render('reg', {
     title: '注册' ,
@@ -25,6 +66,7 @@ router.get('/reg', function(req, res, next) {
   });
 });
 
+router.post('/reg',isNotLogin);
 router.post('/reg',function(req,res,next){
   var name = req.body.name,
       password = req.body.password,
@@ -69,4 +111,21 @@ router.post('/reg',function(req,res,next){
   });
 
 });
+
 module.exports = router;
+
+function isLogin(req,res,next){
+  if(!req.session.user){
+    req.flash('error', '未登录!');
+    res.redirect('/login');
+  }
+  next()
+}
+
+function isNotLogin(req, res, next) {
+  if (req.session.user) {
+    req.flash('error', '已登录!');
+    res.redirect('back');//返回之前的页面
+  }
+  next();
+}
