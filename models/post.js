c06e83f4c2312519578613e666b263b578e80d66
丁强 <dq6663@ly.com>
2015-08-20 09:00:39 +0800

    添加post功能

--- models/post.js
+++ models/post.js
@@ -3,7 +3,7 @@
  */
 var mongodb = require('./db');
 
-function Post(name,title,post){
+function Post(name, title, post) {
     this.name = name;
     this.title = title;
     this.post = post;
@@ -12,17 +12,17 @@ function Post(name,title,post){
 module.exports = Post;
 
 //存储一篇文章及相关信息
-Post.prototype.save = function(callback){
+Post.prototype.save = function (callback) {
     var date = new Date();
 
     //存储各种类型时间(似乎并没有必要)
     var time = {
-        date : date,
-        year : date.getFullYear(),
-        month : date.getFullYear()+'-'+(date.getMonth()+1),
-        day : date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate(),
-        minute : date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate()+' '+date.getHours()+':'+
-        (date.getMinutes()<10?'0'+date.getMinutes():date.getMinutes())
+        date: date,
+        year: date.getFullYear(),
+        month: date.getFullYear() + '-' + (date.getMonth() + 1),
+        day: date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate(),
+        minute: date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ':' +
+        (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes())
     }
 
     //要存入数据库的文档
@@ -33,21 +33,21 @@ Post.prototype.save = function(callback){
         post: this.post
     }
 
-    mongodb.open(function(err,db){
-        if(err){
+    mongodb.open(function (err, db) {
+        if (err) {
             return callback(err);
         }
         //读取posts集合
-        db.collection('posts',function(err,collection){
-            if(err){
+        db.collection('posts', function (err, collection) {
+            if (err) {
                 mongodb.close();
                 return callback(err);
             }
 
             //将文档插入posts集合
-            collection.insert(post,{safe: true},function(err){
+            collection.insert(post, {safe: true}, function (err) {
                 mongodb.close();
-                if(err){
+                if (err) {
                     return callback(err);
                 }
                 callback(null);
@@ -57,28 +57,28 @@ Post.prototype.save = function(callback){
 };
 
 //读取文章及其相关信息
-Post.get = function(name,callback){
+Post.get = function (name, callback) {
     //打开数据库
-    mongodb.open(function(err,db){
-        if(err){
+    mongodb.open(function (err, db) {
+        if (err) {
             return callback(err);
         }
         //读取posts集合
-        db.collection('posts',function(err,collection){
-            if(err){
+        db.collection('posts', function (err, collection) {
+            if (err) {
                 mongodb.close();
                 return callback(err);
             }
             var query = {};
-            if(name){
+            if (name) {
                 query.name = name;
             }
-            collection.find(query).sort({time: -1}).toArray(function(err,docs){
+            collection.find(query).sort({time: -1}).toArray(function (err, docs) {
                 mongodb.close();
-                if(err){
+                if (err) {
                     return callback(err);
                 }
-                callback(null,docs);
+                callback(null, docs);
             });
         });
     });
